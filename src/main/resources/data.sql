99957ece19b87549e1db4cb302aa890a2a424827
Hamza Abdilahi <hamzacodes97@gmail.com>
2022-05-19 14:50:03 +0100

    sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,12 +9,12 @@ insert into PLAYERS (ID, AGE, FIRST_NAME, LAST_NAME, POINTS, POSITION, TEAM_NAME
 insert into PLAYERS (ID, AGE,  FIRST_NAME, LAST_NAME, POINTS,POSITION, TEAM_NAME) values (9, 25, 'Jalen ', 'Brunson', 22.3, 'Guard', 'Dallas Mavericks');
 insert into PLAYERS (ID, AGE, FIRST_NAME, LAST_NAME, POINTS, POSITION, TEAM_NAME) values (10,30, 'CJ ', 'McCollum', 22.2, 'Guard', 'New Orleans Pelicans');
 
-insert into TEAMS (ID, GAMES_PLAYED, LOSES, TEAM_NAMES, WINS, PLAYERS_ID) values (1, );
-insert into TEAMS (ID, GAMES_PLAYED, LOSES, TEAM_NAMES, WINS, PLAYERS_ID) values ();
-insert into TEAMS (ID, GAMES_PLAYED, LOSES, TEAM_NAMES, WINS, PLAYERS_ID) values ();
-insert into TEAMS (ID, GAMES_PLAYED, LOSES, TEAM_NAMES, WINS, PLAYERS_ID) values ();
-insert into TEAMS (ID, GAMES_PLAYED, LOSES, TEAM_NAMES, WINS, PLAYERS_ID) values ();
-insert into TEAMS (ID, GAMES_PLAYED, LOSES, TEAM_NAMES, WINS, PLAYERS_ID) values ();
-insert into TEAMS (ID, GAMES_PLAYED, LOSES, TEAM_NAMES, WINS, PLAYERS_ID) values ();
-insert into TEAMS (ID, GAMES_PLAYED, LOSES, TEAM_NAMES, WINS, PLAYERS_ID) values ();
+insert into TEAMS (ID, GAMES_PLAYED, LOSES, TEAM_NAMES, WINS, PLAYERS_ID) values (2,14, 6, 'Dallas Mavericks', 8, 9);
+insert into TEAMS (ID, GAMES_PLAYED, LOSES, TEAM_NAMES, WINS, PLAYERS_ID) values (3,13, 6, 'Phoenix Suns', 7, 8);
+insert into TEAMS (ID, GAMES_PLAYED, LOSES, TEAM_NAMES, WINS, PLAYERS_ID) values (1,12, 3, 'Golden State Warrior', 9, 5);
+insert into TEAMS (ID, GAMES_PLAYED, LOSES, TEAM_NAMES, WINS, PLAYERS_ID) values (4,12, 6, 'Memphis Grizzlies', 6, 3);
+insert into TEAMS (ID, GAMES_PLAYED, LOSES, TEAM_NAMES, WINS, PLAYERS_ID) values (5,6, 4, 'Minnesota Timberwolves', 2, 7);
+insert into TEAMS (ID, GAMES_PLAYED, LOSES, TEAM_NAMES, WINS, PLAYERS_ID) values (6,6, 4, 'New Orleans Pelicans', 2,4);
+insert into TEAMS (ID, GAMES_PLAYED, LOSES, TEAM_NAMES, WINS, PLAYERS_ID) values (7,6, 4, 'Utah Jazz', 2, 6);
+insert into TEAMS (ID, GAMES_PLAYED, LOSES, TEAM_NAMES, WINS, PLAYERS_ID) values (8,5, 4, 'Denver Nuggest', 1, 1);
 

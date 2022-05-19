cd669174b0150029b8670d2782e93a1726a28fa9
Hamza Abdilahi <hamzacodes97@gmail.com>
2022-05-19 15:28:04 +0100

    fixes

--- src/main/java/com/example/Solo/Spring/Project/controller/PlayersController.java
+++ src/main/java/com/example/Solo/Spring/Project/controller/PlayersController.java
@@ -4,13 +4,13 @@ import com.example.Solo.Spring.Project.model.Players;
 import com.example.Solo.Spring.Project.model.Teams;
 import com.example.Solo.Spring.Project.repository.PlayersRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+
+@RestController
+@RequestMapping("player")
 
 public class PlayersController {
 

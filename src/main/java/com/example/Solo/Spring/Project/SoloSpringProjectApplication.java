aef8adb5dded759efa5c5b20f9d7b8658aefc9bd
Hamza Abdilahi <hamzacodes97@gmail.com>
2022-05-19 09:30:31 +0100

    first commit

---
+++ src/main/java/com/example/Solo/Spring/Project/SoloSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Solo.Spring.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloSpringProjectApplication.class, args);
+	}
+
+}
